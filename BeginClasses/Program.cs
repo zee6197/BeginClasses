46135807f03b410c5dcfa6a71bd7677c7e9e1731
zee6197 <zeeman@berkeley.edu>
2022-05-04 08:25:15 -0700

    First commit

---
+++ BeginClasses/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BeginClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var TheCar = new Car();
+            TheCar.Make = "Audi";
+            TheCar.Model = "A6"; 
+             TheCar.Year = 2016;
+
+            Console.WriteLine($"The Year is: {TheCar.Year}");
+            Console.WriteLine($"The Make is: {TheCar.Make}");
+            Console.WriteLine($"The Model is: {TheCar.Model}");
+
+         }
+
+    }
+
+}
